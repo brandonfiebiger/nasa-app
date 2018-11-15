ea361eb2d1620d80570d1b25910f019523345894
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-11-15 14:22:51 -0700

    Fetch nasa photos and clean them in componentDidMount, set them to apps state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { apiKey } from './keys';
+import { nasaCleaner } from './utils/helper';
 
 class App extends Component {
   constructor() {
@@ -14,26 +15,16 @@ class App extends Component {
   componentDidMount() {
     fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${apiKey}`)
       .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.log(error));
+      .then(data => this.setState({
+        photos: nasaCleaner(data)
+      }))
+      .catch(error => console.log(error))
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        App
       </div>
     );
   }
