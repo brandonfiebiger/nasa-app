c28b500cc31b65a7524783b41b286384e8da7975
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-11-15 19:41:56 -0700

    Add Info component to render rover and camera data.

---
+++ src/Info/Info.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+export const Info = (props) => {
+
+  console.log(props.rover, props.camera)
+  return(
+    <div>
+      <h1>{props.date}</h1>
+      <h2>Rover: {props.rover.name}</h2>
+      <h2>launch date: {props.rover.launch_date}</h2>
+      <h2>landing date: {props.rover.landing_date}</h2>
+      <h3>Camera: {props.camera.name}</h3>
+    </div>
+  )
+}
+
+export default Info;
